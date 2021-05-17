c3820681967ec0b5ba846d7f6cee58974eb2622a
bnv20 <amicobnv20@gmail.com>
2021-05-17 23:05:57 +0900

    add gitignore

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = [BASE_DIR / 'staticfiles']
 STATIC_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
